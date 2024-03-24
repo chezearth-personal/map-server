20457a7cf3f4e53aa478afc58dba7aecfe9b4b03
Charles Rethman <rethman@me.com>
2024-03-25 12:51:37 +1300

    Added in Namibia images

--- client/src/components/MapImage.js
+++ client/src/components/MapImage.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import useImage from '../hooks/useImage';
 
-export default function MapImage(props) {
-  const { loading, error, image } = useImage(props.map.file);
+export default function MapImage({ map }) {
+  const { loading, error, image } = useImage(map.file);
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
   return (
     <div>
             <div className='map'>
-              <img src={image.default} className='hazard_maps' alt={props.map.title} />
+              <img src={image.default} className='hazard_maps' alt={map.title} />
             </div>
             <div className='mapDescriptions'>
-              <h4>{props.map.title}</h4>
-              <p>{props.map.description}</p>
+              <h4>{map.title}</h4>
+              <p>{map.description}</p>
             </div>
     </div>
   );
--- client/src/components/Selector.js
+++ client/src/components/Selector.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-export default function Selector(props) {
+export default function Selector({ maps, select }) {
   return (
     <div className='selector'>
       <label htmlFor='mapToView'>Select the map you wish to view</label>
-      <select id='mapToView' name='mapToView' onChange={e => props.select(e.target)}>
-      {props.maps.length > 0
-        ? props.maps
+      <select id='mapToView' name='mapToView' onChange={e => select(e.target)}>
+      {maps.length > 0
+        ? maps
           .map((map, index) => (
             <option value={map.value} key={index}>{map.title}</option>
           ))
